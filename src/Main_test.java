eb40f2a7cbb93d4527f4ffe552298b14ad100394
fyl_66 <fyl_66@163.com>
2018-07-04 16:04:04 +0800

    16:03分测试提交

--- src/Main_test.java
+++ src/Main_test.java
@@ -7,6 +7,7 @@
 public class Main_test {
     public static void main(String[] args) {
         System.out.println("hello world!");
-
+        System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
