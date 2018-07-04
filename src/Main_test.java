97aac196e17c10d56f936fedde1b8d5c45cac4cd
fyl_66 <fyl_66@163.com>
2018-07-04 15:52:56 +0800

    测试

---
+++ src/Main_test.java
@@ -0,0 +1,11 @@
+/**
+ * ≤‚ ‘Ã·Ωª
+ *
+ * @author :fyl_66@163.com
+ * @create 2018-07-04 15:50
+ **/
+public class Main_test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
